9184658b31697d09e693dfa23a85cdd05f92da0e
MeanBordin <bodinsak2544@gmail.com>
2023-11-10 21:12:26 +0700

    add cors config

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const cors = require('cors')
 // middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:4200',  // กำหนดโดเมนที่ได้รับอนุญาต
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // กำหนด HTTP methods ที่อนุญาต
+  credentials: true, // อนุญาตการส่งค่า Cookie ร่วมกับ requests
+  optionsSuccessStatus: 204, // กำหนด HTTP status code สำหรับการ preflight request
+  allowedHeaders: 'Content-Type,Authorization', // กำหนด HTTP headers ที่อนุญาต
+}))
 app.use('/images', express.static('images')) //กำหนดชื่อ path และ เข้าถึง folder images
 
 // router
